fc6e2e8f1b0b1eaae7d693bc2dd5a0c0c1561acb
lucapolzone <lucapolzone@gmail.com>
2024-02-28 17:33:35 +0100

    feat: add php logic for generated password

--- index.php
+++ index.php
@@ -1,19 +1,47 @@
 <?php
 
-  $form_sent = !empty($_GET);
+$form_sent = !empty($_GET);
 
-  if ($form_sent) {
-    var_dump($_GET);
+if ($form_sent) {
+  // var_dump($_GET);
+
+  $password_length = $_GET['password-length'];
+
+
+  //lettere minuscole
+  $letters = 'abcdefghijklmnopqrstuvwxyz';
+  //lettere maiuscole
+  $up_letters = strtoupper($letters);
+  //simboli
+  $symbols = '!"#$%&\'()*+<=>?@[\\]^_`{|}~';
+  //numeri
+  $numbers = '0123456789';
+
+  $generated_password = '';
+
+
+  while (strlen($generated_password) < $password_length) {
+    //concatenazione di tutte le stringhe
+    $all_characters = $letters . $up_letters . $symbols . $numbers;
+    // echo $all_characters;
+
+    //prendo un carattere random da 0 all'ultimo indice valido
+    $random_character = $all_characters[rand(0, strlen($all_characters) - 1)];
+
+    $generated_password .= $random_character;;
   }
 
 
-
+  echo $generated_password;
+}
 
 ?>
 
 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -21,11 +49,11 @@
   <meta name="description" content="Esercizio - PHP Strong Password Generator" />
   <title>Esercizio - PHP Strong Password Generator</title>
   <!-- Bootstrap link -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-  integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <!-- CSS link -->
   <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
   <div class="container mt-5">
     <h1 class="fw-bold text-center">Strong Password Generator</h1>
@@ -35,7 +63,7 @@
         <div class="alert alert-success opacity-0">
           <p>&nbsp;</p>
         </div>
-      </div>  
+      </div>
       <div class="col-12">
         <div class="card mt-1">
           <div class="p-4">
@@ -53,4 +81,5 @@
     </div>
   </div>
 </body>
+
 </html>

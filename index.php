80d7570519273cc6301514ba11c843d9368b17c5
lucapolzone <lucapolzone@gmail.com>
2024-02-28 17:39:42 +0100

    feat: add php logic in a function. milestone 1 completed

--- index.php
+++ index.php
@@ -7,32 +7,40 @@ if ($form_sent) {
 
   $password_length = $_GET['password-length'];
 
+  function random_password($password_length) {
 
-  //lettere minuscole
-  $letters = 'abcdefghijklmnopqrstuvwxyz';
-  //lettere maiuscole
-  $up_letters = strtoupper($letters);
-  //simboli
-  $symbols = '!"#$%&\'()*+<=>?@[\\]^_`{|}~';
-  //numeri
-  $numbers = '0123456789';
+    //lettere minuscole
+    $letters = 'abcdefghijklmnopqrstuvwxyz';
+    //lettere maiuscole
+    $up_letters = strtoupper($letters);
+    //simboli
+    $symbols = '!"#$%&\'()*+<=>?@[\\]^_`{|}~';
+    //numeri
+    $numbers = '0123456789';
 
-  $generated_password = '';
+    $generated_password = '';
 
 
-  while (strlen($generated_password) < $password_length) {
-    //concatenazione di tutte le stringhe
-    $all_characters = $letters . $up_letters . $symbols . $numbers;
-    // echo $all_characters;
+    while (strlen($generated_password) < $password_length) {
+      //concatenazione di tutte le stringhe
+      $all_characters = $letters . $up_letters . $symbols . $numbers;
+      // echo $all_characters;
 
-    //prendo un carattere random da 0 all'ultimo indice valido
-    $random_character = $all_characters[rand(0, strlen($all_characters) - 1)];
+      //prendo un carattere random da 0 all'ultimo indice valido
+      $random_character = $all_characters[rand(0, strlen($all_characters) - 1)];
 
-    $generated_password .= $random_character;;
+      $generated_password .= $random_character;;
+    }
+
+    return $generated_password;
+
   }
 
 
-  echo $generated_password;
+  $password = random_password($password_length);
+  echo $password;
+
+
 }
 
 ?>
